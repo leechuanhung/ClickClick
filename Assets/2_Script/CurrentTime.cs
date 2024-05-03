93dc81493270431210d913de3cb6ee60b9252364
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-05-03 14:17:18 +0900

    수행 당일

--- Assets/2_Script/CurrentTime.cs
+++ Assets/2_Script/CurrentTime.cs
@@ -3,9 +3,20 @@ using UnityEngine.UI;
 
 public class CurrentTime : MonoBehaviour
 {
+    public static float bestTime = 0;
+
     void Start()
     {
-        GetComponent<Text>().text = 
+        GetComponent<Text>().text = "Best Time : " + Time.time.ToString();
+        bestTime = 0;
     }
 
+
+    void Update()
+    {
+        if (Time.time > bestTime)
+        {
+            Time.captureDeltaTime = bestTime;
+        }
+    }
 }
